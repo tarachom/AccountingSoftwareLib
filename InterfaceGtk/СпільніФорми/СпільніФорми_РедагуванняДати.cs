8371ac606068dc8d6005f4be4536dd677cf08110
Yura <tarachom@gmail.com>
2025-05-22 12:44:09 +0300

    Редагування датичасу у таб частині

--- InterfaceGtk/СпільніФорми/СпільніФорми_РедагуванняДати.cs
+++ InterfaceGtk/СпільніФорми/СпільніФорми_РедагуванняДати.cs
@@ -33,17 +33,97 @@ namespace InterfaceGtk
 {
     public class СпільніФорми_РедагуванняДати : ФормаСпливаючеВікно
     {
-        DateTimeControl date = new DateTimeControl();
+        Calendar calendar = new Calendar();
+        SpinButton hourSpin = new SpinButton(0, 23, 1) { Orientation = Orientation.Vertical };
+        SpinButton minuteSpin = new SpinButton(0, 59, 1) { Orientation = Orientation.Vertical };
+        SpinButton secondSpin = new SpinButton(0, 59, 1) { Orientation = Orientation.Vertical };
 
         public System.Action<DateTime>? CallBack_Update = null;
 
         public СпільніФорми_РедагуванняДати()
         {
-            //Текст
+            //Календар
             {
                 Box hBox = new Box(Orientation.Horizontal, 0);
                 PackStart(hBox, true, true, 0);
-                hBox.PackStart(date, true, true, 0);
+
+                Box vBox = new Box(Orientation.Vertical, 0);
+                hBox.PackStart(vBox, false, false, 0);
+
+                calendar.DaySelected += (sender, args) =>
+                {
+                    try
+                    {
+                        Value = new DateTime(calendar.Date.Year, calendar.Date.Month, calendar.Date.Day,
+                            Value.Hour, Value.Minute, Value.Second);
+                    }
+                    catch
+                    {
+                        Value = DateTime.MinValue;
+                    }
+                };
+
+                calendar.DaySelectedDoubleClick += (sender, args) =>
+                {
+                    CallBack_Update?.Invoke(this.Value);
+                    PopoverParent?.Hide();
+                };
+
+                vBox.PackStart(calendar, false, false, 0);
+
+                Box hBoxTime = new Box(Orientation.Horizontal, 0) { Halign = Align.Center };
+                vBox.PackStart(hBoxTime, false, false, 5);
+
+                //Hour
+                {
+                    hourSpin.ValueChanged += (sender, args) => Value = new DateTime(
+                        calendar.Date.Year, calendar.Date.Month, calendar.Date.Day,
+                        (int)hourSpin.Value, Value.Minute, Value.Second);
+
+                    hBoxTime.PackStart(hourSpin, false, false, 0);
+                }
+
+                hBoxTime.PackStart(new Label(":"), false, false, 5);
+
+                //Minute
+                {
+                    minuteSpin.ValueChanged += (sender, args) => Value = new DateTime(
+                        calendar.Date.Year, calendar.Date.Month, calendar.Date.Day,
+                        Value.Hour, (int)minuteSpin.Value, Value.Second);
+
+                    hBoxTime.PackStart(minuteSpin, false, false, 0);
+                }
+
+                hBoxTime.PackStart(new Label(":"), false, false, 5);
+
+                //Second
+                {
+                    secondSpin.ValueChanged += (sender, args) => Value = new DateTime(
+                        calendar.Date.Year, calendar.Date.Month, calendar.Date.Day,
+                        Value.Hour, Value.Minute, (int)secondSpin.Value);
+
+                    hBoxTime.PackStart(secondSpin, false, false, 0);
+                }
+
+                //Поточна дата
+                {
+                    LinkButton lbCurrentDate = new LinkButton("", "Поточна дата");
+                    lbCurrentDate.Clicked += (sender, args) =>
+                    {
+                        calendar.Date = Value = DateTime.Now;
+
+                        hourSpin.Value = TimeOnly.FromDateTime(Value).Hour;
+                        minuteSpin.Value = TimeOnly.FromDateTime(Value).Minute;
+                        secondSpin.Value = TimeOnly.FromDateTime(Value).Second;
+                    };
+
+                    vBox.PackStart(lbCurrentDate, false, false, 0);
+                }
+            }
+
+            //Сепаратор
+            {
+                PackStart(new Separator(Orientation.Horizontal), false, false, 2);
             }
 
             //Кнопка
@@ -61,15 +141,28 @@ namespace InterfaceGtk
             }
         }
 
+        bool isInit = true;
+
+        DateTime _Value;
         public DateTime Value
         {
             get
             {
-                return date.Value;
+                return _Value;
             }
             set
             {
-                date.Value = value;
+                _Value = value;
+
+                if (isInit)
+                {
+                    isInit = false;
+
+                    calendar.Date = _Value;
+                    hourSpin.Value = TimeOnly.FromDateTime(_Value).Hour;
+                    minuteSpin.Value = TimeOnly.FromDateTime(_Value).Minute;
+                    secondSpin.Value = TimeOnly.FromDateTime(_Value).Second;
+                }
             }
         }
     }
